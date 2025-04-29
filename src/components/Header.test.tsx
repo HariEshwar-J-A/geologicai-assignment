35a6601c5cc2e6734714c08448f6afd67f719507
Harieshwar J A <harirmdeee@gmail.com>
2025-04-29 18:11:57 -0400

    feat(ui): :sparkles: update header component and corresponding test file

--- src/components/Header.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Header from './Header/Header';
-
-describe('Header', () => {
-  it('renders dark-mode toggle button', () => {
-    render(<Header />);
-    expect(screen.getByRole('button', { name: /dark mode/i })).toBeInTheDocument();
-  });
-});
