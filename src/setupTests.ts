606836c633de41897280eeda2a26ac6fc95b2986
Harieshwar J A <harirmdeee@gmail.com>
2025-04-29 18:28:07 -0400

    test: :clown_face: add polyfill for non-browser based test env

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,11 @@
 import '@testing-library/jest-dom';
+
+if (!window.URL.createObjectURL) {
+  Object.defineProperty(window.URL, 'createObjectURL', {
+    configurable: true,
+    writable: true,
+    value: () => {
+      return 'blob:mock';
+    },
+  });
+}
