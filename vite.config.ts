ae252a2cc85227c03e2362bdeca763e4d34edc3a
Harieshwar J A <harirmdeee@gmail.com>
2025-04-30 12:28:08 -0400

    ci: :zap: improve vite build config for better chunking

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  test: {
-    environment: 'jsdom',
-  }
-})
+  build: {
+    target: "esnext",
+    chunkSizeWarningLimit: 2000, // bump the warning so you don’t get spammed at 500k
+  },
+  resolve: {
+    alias: {
+      web3: 'web3/dist/web3.min.js',
+    },
+  },
+  base: "/geologicai-assignment"
+});
